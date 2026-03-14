488e83f441f5915899ed83e6362449bb2e44a314
Kalleh03 <kalle.holmgren@proton.me>
2026-03-14 15:35:31 +0100

    first diary entry

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+
+
+class Program
+{   
+    
+
+    static void Main()
+    {
+        
+        string name = "kalle";
+        decimal date = 14.3m;
+        int year = 26;
+
+        Console.WriteLine($"Hello! my name is, {name} todays date is: {date} - {year}!");
+        Console.WriteLine("The journey has begun and it's going pretty well actually.");
+        Console.WriteLine("I have learned to declare int and string and to interpolate them in CW lines");
+        Console.WriteLine("Decimals and what to put after them still baffles me but i'm getting there. it's 'm' btw");
+        Console.WriteLine("I am using GIT to learn how it works and diary my own learning path.");
+        Console.WriteLine("Right now i feel like i have my hand in 12 different cans of jam. Not ideal, but it works.");
+
+    }
+}
