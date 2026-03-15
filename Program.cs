fed05b7ad744cedb6fb88fab3c0798a95cdc961b
Kalleh03 <kalle.holmgren@proton.me>
2026-03-15 17:48:38 +0100

    Second commit, arrays are messing with me.

--- Program.cs
+++ Program.cs
@@ -9,17 +9,15 @@ class Program
 
     static void Main()
     {
-        
-        string name = "kalle";
-        decimal date = 14.3m;
-        int year = 26;
-
-        Console.WriteLine($"Hello! my name is, {name} todays date is: {date} - {year}!");
-        Console.WriteLine("The journey has begun and it's going pretty well actually.");
-        Console.WriteLine("I have learned to declare int and string and to interpolate them in CW lines");
-        Console.WriteLine("Decimals and what to put after them still baffles me but i'm getting there. it's 'm' btw");
-        Console.WriteLine("I am using GIT to learn how it works and diary my own learning path.");
-        Console.WriteLine("Right now i feel like i have my hand in 12 different cans of jam. Not ideal, but it works.");
+        string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+       
+        foreach (string orderID in orderIDs)
+        {
+            if (orderID.StartsWith("B"))
+            {
+                Console.WriteLine(orderID);
+            }
+        }
 
     }
 }
