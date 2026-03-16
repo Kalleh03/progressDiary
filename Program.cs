724c983a6070a5297f46a78c0c34cfc2823d0e77
Kalleh03 <kalle.holmgren@proton.me>
2026-03-16 15:34:46 +0100

    updating progress on arrays, not going well.

--- Program.cs
+++ Program.cs
@@ -9,9 +9,12 @@ class Program
 
     static void Main()
     {
-        string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
-       
-        foreach (string orderID in orderIDs)
+        string[] orderIDs = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+        int [] orderNr  = {111, 444, 666, 442, 555, 888, 145, 135, 1113};
+        int sum = 0;
+        int bin = 0;
+        
+       foreach(string orderID in orderIDs)
         {
             if (orderID.StartsWith("B"))
             {
@@ -19,5 +22,22 @@ class Program
             }
         }
 
+        foreach (int order in orderNr)
+        {
+            
+            sum += order;
+            bin++;
+            Console.WriteLine($"Bin {bin} = {order} items (running total: {sum})");
+            
+            
+        }
+        Console.WriteLine($"We have {sum} items in inventory.");
+
     }
 }
+
+
+// först gör man en string med orderIDs. 
+// sen gör man en foreach med string orderID som pekar in i string med in orderIDs
+// efter det en if orderID.StartsWith
+// sen skriver man ut orderID
