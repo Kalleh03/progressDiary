35903a2d3b327e59208fddd8e586c85e03ae540f
Kalle Holmgren <kalle.holmgren@proton.me>
2026-04-07 18:08:05 +0200

    if statements for permissions

--- Program.cs
+++ Program.cs
@@ -1,3 +1,28 @@
 ﻿
-Random coin = new Random();
-Console.WriteLine((coin.Next(0, 2) == 0) ? "The coin says: Heads" : "The coin says: Tails");
+
+string permission = "Admin|Manager";
+int level = 55;
+
+// ett if statement med permission först för att se om man är admin eller inte. 
+// sen ett if statement med level för admin och else
+// sen fortsätta så ner i listan med level.
+
+if (level > 55 && permission.Contains("Admin"))
+{
+    Console.WriteLine("Welcome, super Admin user");
+}
+
+else if(level >= 20 && permission.Contains("Admin"))
+{
+    Console.WriteLine("Welcome, Admin user.");
+}
+
+else if(level <= 20 && permission.Contains("Admin"))
+{
+    Console.WriteLine("Contact an Admin for access.");
+}
+
+else 
+{
+    Console.WriteLine("You do not have sufficient privileges");
+}
--- obj/Debug/net10.0/1.AssemblyInfo.cs
+++ obj/Debug/net10.0/1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+905e3767f290f323c92202ff2c91da9f82ab759c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cb1d62f13ab762c7cab433874eba56da62f5b6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
