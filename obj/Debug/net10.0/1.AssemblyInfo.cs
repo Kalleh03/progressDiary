4cb1d62f13ab762c7cab433874eba56da62f5b6a
Kalle Holmgren <kalle.holmgren@proton.me>
2026-04-07 17:01:01 +0200

    used Random() to make a short coinflip game.

--- obj/Debug/net10.0/1.AssemblyInfo.cs
+++ obj/Debug/net10.0/1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed779a0fa81486499ed741b90736ce0a1c570687")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+905e3767f290f323c92202ff2c91da9f82ab759c")]
 [assembly: System.Reflection.AssemblyProductAttribute("1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
