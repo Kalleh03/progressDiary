75c3880de5498a720172e35271bc4cdbe75f043c
Kalle Holmgren <kalle.holmgren@proton.me>
2026-04-08 20:38:14 +0200

    more switch statements!

--- Program.cs
+++ Program.cs
@@ -1,37 +1,34 @@
 ﻿
 
 // vi börjar med att declare employeelevel och employeename.
-int employeelevel = 400;
-string employeename = "Kalle";
+int employeLevel = 300;
+string employeName = "Kalle";
 
 // sen behöver vi en tom string för title att använda.
 
 string title = "";
 
+
 // sen switch block för employeelevel och case inuti med title och sist en break för att bryta process.
 // ett switch statement är ett block och behöver inte delas med {}. varje case avslutas med ":". sista heter default.
 
-switch (employeelevel)
+switch (employeLevel)
 {
-    case 100:
-    title = "Junior associate";
-    break;
+case 100:
+title = "Mupp";
+break;
 
-    case 200:
-    title = "Medium associate";
-    break;
+case 200:
+title = "Muppare";
+break;
 
-    case 300:
-    title = "Senior associate";
-    break;
+case 300:
+title = "Muppast";
+break;
 
-    case 400:
-    title = "Owner";
-    break;
-
-    default:
-    title = "Scrub";
-    break;
+default:
+title = "El-reta..";
+break;
 }
 
-Console.WriteLine($"{employeename} is a {title}");
+Console.WriteLine($"{employeName} is {title}");
--- obj/Debug/net10.0/1.AssemblyInfo.cs
+++ obj/Debug/net10.0/1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a41059a0577b017670b88c406ed43a37d5b7630a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd63d1178b7dbadb82271f99cae4262bd38809d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
