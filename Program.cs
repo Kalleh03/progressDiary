a7f751cd00e54c0a930149ca33fc651ee3af6450
Kalle Holmgren <kalle.holmgren@proton.me>
2026-04-09 15:20:33 +0200

    switch statements suck the life from me.

--- Program.cs
+++ Program.cs
@@ -10,63 +10,6 @@ string type = "";
 string color = "";
 string size = "";
 
-string clothes = "";
 
-// gjort en ny string clothes för att spara kläder.  Nu visar den "M" för mitt case är tomt. 
-
-switch(clothes)
-{
-    case "product":
-    size = "S";
-    break;
-
-    case "":
-    size = "M";
-    break;
-
-    case "03":
-    size = "L";
-    break;
-}
 
 Console.WriteLine($"Product: {size} {color} {type}");
-
-// if (product[0] == "01")
-// {
-//     type = "Sweat shirt";
-// } else if (product[0] == "02")
-// {
-//     type = "T-Shirt";
-// } else if (product[0] == "03")
-// {
-//     type = "Sweat pants";
-// }
-// else
-// {
-//     type = "Other";
-// }
-
-// if (product[1] == "BL")
-// {
-//     color = "Black";
-// } else if (product[1] == "MN")
-// {
-//     color = "Maroon";
-// } else
-// {
-//     color = "White";
-// }
-
-// if (product[2] == "S")
-// {
-//     size = "Small";
-// } else if (product[2] == "M")
-// {
-//     size = "Medium";
-// } else if (product[2] == "L")
-// {
-//     size = "Large";
-// } else
-// {
-//     size = "One Size Fits All";
-// }
--- obj/Debug/net10.0/1.AssemblyInfo.cs
+++ obj/Debug/net10.0/1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75c3880de5498a720172e35271bc4cdbe75f043c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff3d8155c83470b2b7c6cabfc6e55e803161515d")]
 [assembly: System.Reflection.AssemblyProductAttribute("1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
