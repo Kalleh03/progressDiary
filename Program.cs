0810d2e647302a5524b25373373f734a5c7574aa
Kalle Holmgren <kalle.holmgren@proton.me>
2026-04-09 18:44:58 +0200

    they put a sneaky array in there... goddammit.

--- Program.cs
+++ Program.cs
@@ -10,6 +10,57 @@ string type = "";
 string color = "";
 string size = "";
 
+switch (product[0])
+{
+    case "01":
+    type = "Sweat shirt";
+    break;
 
+    case "02":
+    type = "T-shirt";
+    break;
 
-Console.WriteLine($"Product: {size} {color} {type}");
+    case "03":
+    type = "Sweat pants";
+    break;
+
+    default:
+    type = "Other";
+    break;
+}
+
+switch (product[1])
+{
+    case "BL":
+    color = "Black";
+    break;
+
+    case "MN":
+    color = "Maroon";
+    break;
+
+    default:
+    color = "White";
+    break;
+}
+
+switch (product[2])
+{
+    case "S":
+    size = "Small";
+    break;
+
+    case "M":
+    size = "Medium";
+    break;
+
+    case "L":
+    size = "Large";
+    break;
+
+    default:
+    size = "Large";
+    break;
+}
+
+Console.WriteLine($"Product: {type} {size} {color} ");
