f17e576e439415895c6239b571b3bf30507d9ee6
Kalle Holmgren <kalle.holmgren@proton.me>
2026-04-12 17:21:02 +0200

    even better formatting

--- obj/Debug/net10.0/1.AssemblyInfo.cs
+++ obj/Debug/net10.0/1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46d1c61ddf6694087de0595b3e741024b1c143ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+681d054a2fbc1714ec7206a6a4e9d59a701a1935")]
 [assembly: System.Reflection.AssemblyProductAttribute("1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
