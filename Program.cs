2dc9eb58964eb21c86f9b8d0ef9753c3bd2bc406
Kalle Holmgren <kalle.holmgren@proton.me>
2026-04-15 18:09:21 +0200

    Building games! I'm not a expert yet...

--- Program.cs
+++ Program.cs
@@ -1,30 +1,26 @@
-﻿
-
-
-
-
-
+﻿// This one eludes me still, the logic behind saving hp and displaying random is still odd. 
+// The same can be said of the while loop which i haven't used much yet. 
+// Perhaps that's the problem... 
 
 int playerHealth = 10;
 int npcHealth = 10;
 
-Console.WriteLine($"Player Health: {playerHealth}");
-Console.WriteLine($"NPC Health: {npcHealth}");
+
 
 Random dice = new Random();
 
 do
 {   
-    int roll = dice.Next(1,11);
-    npcHealth -= roll;
-    Console.WriteLine($"Monster takes damage of {roll} health and now has {npcHealth} health");    
-    
-    if (npcHealth <= 0) continue;
+   int roll = dice.Next(1,11);
+   npcHealth -= roll;
+   Console.WriteLine($"Monster is hit, takes {roll} damage. Monster has {npcHealth}HP left.");
+   if (npcHealth <= 0) continue;
 
-    roll = dice.Next(1,11);
-    playerHealth -= roll;
-    Console.WriteLine($"Player takes damage of {roll} and now has {playerHealth} health.");
+   roll = dice.Next(1,11);
+   playerHealth -= roll;
+   Console.WriteLine($"Player is hit, takes {roll} damage. Hero has {playerHealth}HP left.");
+   
 
-} while (npcHealth > 0 && npcHealth > 0);
+} while (npcHealth > 0 && playerHealth > 0);
 
-Console.WriteLine(playerHealth > npcHealth ? "Player wins!" : "Monster wins!");
+Console.WriteLine(playerHealth > npcHealth ? "Hero wins!" : "Monster wins!");
