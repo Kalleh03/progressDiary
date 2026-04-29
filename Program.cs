251ab1efd3258347313673c86514331130f91242
Kalle Holmgren <kalle.holmgren@proton.me>
2026-04-29 16:22:09 +0200

    small progress, don't get it yet.

--- Program.cs
+++ Program.cs
@@ -1,14 +1,29 @@
-﻿// I feel like i have understood how to convert from string, to double, to decimal and back to int again without problems. 
+﻿//  I feel like the Microsoft course is complicating the conversions something  terrible and i'm working on a way around it. 
 
 
-Console.Write("Write a number: ");
-int number = Convert.ToInt32(Console.ReadLine());
-double numberDouble = Convert.ToDouble(number);
-Console.WriteLine($"{numberDouble} double");
-Console.WriteLine(numberDouble.GetType());
-decimal numberDecimal = Convert.ToDecimal(numberDouble);
-Console.WriteLine($"{numberDecimal} decimal");
-Console.WriteLine(numberDecimal.GetType());
-int number2 = Convert.ToInt32(numberDecimal);
-Console.WriteLine($"{number2} int again!");
-Console.WriteLine(number2.GetType());
+
+
+
+string userInput = "";
+int userinputInt = Convert.ToInt32(userInput);
+bool validEntry = false;
+
+Console.WriteLine("Write a number between 5 and 10:");
+
+do 
+{      
+    userInput = Console.ReadLine();
+    if (validEntry != null)
+    {
+        if (userinputInt >= 5 || userinputInt <= 10)
+        {
+            validEntry = false;
+        }    
+
+
+
+    }    
+    
+} while (validEntry == false);
+
+Console.WriteLine($"Your input value {userinputInt} has been accepted!");
--- obj/Debug/net10.0/1.AssemblyInfo.cs
+++ obj/Debug/net10.0/1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ba0e7086511972d3e1247ca0cdd99d513f4ac15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1cf26ec15c545af79f7f4e586372f4216744537")]
 [assembly: System.Reflection.AssemblyProductAttribute("1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
