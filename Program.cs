7e7e6f1b7a686e84df47a7f8ace6024a3436cd2d
Kalle Holmgren <kalle.holmgren@proton.me>
2026-05-03 15:59:05 +0200

    Goddamn do while loops.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿//  do loops haunt me. 
+﻿//  I still don't get the order of doing things with bools.   
 
 
 
@@ -7,12 +7,41 @@
 // While loops checks the condition before executing anything.
 // do while loops checks the condition at the end so the code within will run once. 
 
-int index = 0;
 
-while (index <= 5)
+string? readResult;
+string valueEntered = "";
+int numValue = 0;
+bool validNumber = false;
+
+Console.WriteLine("Enter an integer value between 5 and 10");
+
+do
 {
-    Console.WriteLine(index);
-    index++;
+    readResult = Console.ReadLine();
+    if (readResult != null) 
+    {
+        valueEntered = readResult;
+    }
 
-}
+    validNumber = int.TryParse(valueEntered, out numValue);
+
+    if (validNumber == true)
+    {
+        if (numValue <= 4 || numValue >= 9)
+        {
+            validNumber = false;
+            Console.WriteLine($"You entered {numValue}. Please enter a number between 5 and 10.");
+        }
+    }
+    else 
+    {
+        Console.WriteLine("Sorry, you entered an invalid number, please try again");
+    }
+} while (validNumber == false);
+
+Console.WriteLine($"Your input value ({numValue}) has been accepted.");
+
+readResult = Console.ReadLine();
+
+
 
--- obj/Debug/net10.0/1.AssemblyInfo.cs
+++ obj/Debug/net10.0/1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+011beb1a6ef9da572bc82d761413ef279ef17f4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0cc55bf28e014316c001c97f763f10372de7e80")]
 [assembly: System.Reflection.AssemblyProductAttribute("1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
