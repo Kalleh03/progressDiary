9b563178c595114786c618a07a1c837372a0621c
Kalle Holmgren <kalle.holmgren@proton.me>
2026-05-04 16:37:33 +0200

    aaargh! bools.

--- Program.cs
+++ Program.cs
@@ -7,41 +7,4 @@
 // While loops checks the condition before executing anything.
 // do while loops checks the condition at the end so the code within will run once. 
 
-
-string? readResult;
-string valueEntered = "";
-int numValue = 0;
-bool validNumber = false;
-
-Console.WriteLine("Enter an integer value between 5 and 10");
-
-do
-{
-    readResult = Console.ReadLine();
-    if (readResult != null) 
-    {
-        valueEntered = readResult;
-    }
-
-    validNumber = int.TryParse(valueEntered, out numValue);
-
-    if (validNumber == true)
-    {
-        if (numValue <= 4 || numValue >= 9)
-        {
-            validNumber = false;
-            Console.WriteLine($"You entered {numValue}. Please enter a number between 5 and 10.");
-        }
-    }
-    else 
-    {
-        Console.WriteLine("Sorry, you entered an invalid number, please try again");
-    }
-} while (validNumber == false);
-
-Console.WriteLine($"Your input value ({numValue}) has been accepted.");
-
-readResult = Console.ReadLine();
-
-
-
+// TryParse always returns a BOOL! who knew, not Microsoft Learn.. that's for sure. 
